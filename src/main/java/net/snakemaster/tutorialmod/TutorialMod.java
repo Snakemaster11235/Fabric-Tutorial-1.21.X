0cc5c397f45cca2c5767aaa4c7e98cef2bf5a4ba
Snakemaster11235 <yugi3512@hotmail.com>
2026-09-10 21:04:57 -0400

    Added Custom Items

--- src/main/java/net/snakemaster/tutorialmod/TutorialMod.java
+++ src/main/java/net/snakemaster/tutorialmod/TutorialMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.util.Identifier;
 
+import net.snakemaster.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +15,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
