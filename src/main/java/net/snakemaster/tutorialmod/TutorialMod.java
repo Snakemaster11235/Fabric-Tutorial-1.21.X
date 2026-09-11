346b638444c20e18d9e98a27dbfa25719384f6ed
Snakemaster11235 <yugi3512@hotmail.com>
2026-09-11 16:36:16 -0400

    Added Custom Block

--- src/main/java/net/snakemaster/tutorialmod/TutorialMod.java
+++ src/main/java/net/snakemaster/tutorialmod/TutorialMod.java
@@ -4,17 +4,18 @@ import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.util.Identifier;
 
+import net.snakemaster.tutorialmod.block.ModBlocks;
 import net.snakemaster.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
