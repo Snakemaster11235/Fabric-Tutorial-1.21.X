3785bdd345590274a0036b236aa33b4ef1aa225d
Snakemaster11235 <yugi3512@hotmail.com>
2026-09-13 17:47:15 -0400

    Added recipes for Raw Pink Garnet and Raw Pink Garnet Block.

--- src/main/java/net/snakemaster/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/snakemaster/tutorialmod/block/ModBlocks.java
@@ -17,18 +17,18 @@ import net.snakemaster.tutorialmod.TutorialMod;
 
 public class ModBlocks {
     public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
-                 new Block(AbstractBlock.Settings.create().strength(4f)
+                 new Block(AbstractBlock.Settings.create().strength(5f)
                          .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
     public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
-            new Block(AbstractBlock.Settings.create().strength(4f)
+            new Block(AbstractBlock.Settings.create().strength(5f)
                     .requiresTool().sounds(BlockSoundGroup.AMETHYST_CLUSTER)));
 
     public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
             new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
-                    AbstractBlock.Settings.create().strength(3f).requiresTool()));
+                    AbstractBlock.Settings.create().strength(4f).requiresTool()));
     public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
             new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
-                    AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+                    AbstractBlock.Settings.create().strength(5f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
